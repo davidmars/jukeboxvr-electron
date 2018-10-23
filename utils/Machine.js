4f59fcdd498e5088bf6e31d73f7d8eabe38408cd
davidmars <d.marsalone@gmail.com>
2018-10-23 05:49:02 +0200

    Machine travaille avec le proc id et nom plus l'adressse MAC + events

--- utils/Machine.js
+++ utils/Machine.js
@@ -1,22 +1,35 @@
 const os = require('os');
 const fs = require('fs');
+const EventEmitter = require("event-emitter-es6");
 /**
  * Permet d'obtenir l'adresse MAC et le nom de l'ordi
  */
-class Machine {
+class Machine extends EventEmitter{
     constructor(){
+        super();
         let me=this;
+
         /**
          * L'adresse mac
          * @type {string}
          */
-        this.macAddress="";
+        this.machineId="";
         /**
          * Le nom de l'ordi
          * @type {string}
          */
         this.name=os.hostname();
 
+        var exec = require('child_process').exec;
+        let test=exec("wmic CPU get ProcessorId",function(error, stdout, stderr){
+            let regex = /[\n]([A-Z0-9]*)/m;
+            let m;
+            if ((m = regex.exec(stdout)) !== null) {
+                console.log("test",m[1]);
+                me.machineId=m[1];
+            }
+            me.emit("EVENT_READY");
+        });
         /**
          * Chemin vers le dossier racine pour le stockage
          * @type {string}
@@ -25,20 +38,22 @@ class Machine {
         if (!fs.existsSync(me.appStoragePath)) {
             fs.mkdirSync(me.appStoragePath);
         }
-
+        /*
         let networks=os.networkInterfaces();
+        console.log("networkInterfaces",networks);
         Object.keys(networks).forEach(function(key,index) {
             let net=networks[key];
             for(let adr of net){
                 if(!adr.internal){
                     if(adr.mac){
-                        me.macAddress=adr.mac;
+                        me.machineId=adr.mac;
                         break;
                     }
                 }
 
             }
         });
+        */
     }
 }
 module.exports = Machine;
